e31d0b06a18b663627b0984c963f143f59084a2c
tom <tugatom@gmail.com>
2019-12-02 17:16:37 -0800

    Adds some initial routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LeagueWrapperComponent } from './league/league-wrapper/league-wrapper.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'league', component: LeagueWrapperComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
