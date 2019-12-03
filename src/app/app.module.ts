c8b17f429c0012016d211bd5a7c6ca739b12192e
tom <tugatom@gmail.com>
2019-12-02 17:55:17 -0800

    Revert "Adds league-wrapper and user-overview components"
    This reverts commit cd1537d5dc69a38af733e6045c49e477940688c0.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserOverviewComponent } from './league/user-overview/user-overview.component';
-import { LeagueWrapperComponent } from './league/league-wrapper/league-wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UserOverviewComponent,
-    LeagueWrapperComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
