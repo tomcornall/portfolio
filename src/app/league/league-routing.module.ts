ba8c4d3091eaab727588da645b834f02c6caa2ae
tom <tugatom@gmail.com>
2019-12-09 13:48:16 -0800

    Sets :summonerName URL parameter in router

--- src/app/league/league-routing.module.ts
+++ src/app/league/league-routing.module.ts
@@ -2,8 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LeagueComponent } from './league.component';
+import { UserOverviewComponent } from './user-overview/user-overview.component';
 
-const routes: Routes = [{ path: '', component: LeagueComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: LeagueComponent
+  },
+  {
+    path: ':summonerName',
+    component: UserOverviewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
