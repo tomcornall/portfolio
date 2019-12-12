a569ab2d3184253e85d16f440bf913ab89c7a97b
tom <tugatom@gmail.com>
2019-12-12 11:44:28 -0800

    Installs time-ago-pipe npm package, uses in match component

--- src/app/league/league.module.ts
+++ src/app/league/league.module.ts
@@ -6,10 +6,10 @@ import { LeagueRoutingModule } from './league-routing.module';
 import { LeagueComponent } from './league.component';
 import { UserOverviewComponent } from './user-overview/user-overview.component';
 import { MatchComponent } from './match/match.component';
-
+import { TimeAgoPipe } from 'time-ago-pipe';
 
 @NgModule({
-  declarations: [LeagueComponent, UserOverviewComponent, MatchComponent],
+  declarations: [LeagueComponent, UserOverviewComponent, MatchComponent, TimeAgoPipe],
   imports: [
     CommonModule,
     LeagueRoutingModule,
