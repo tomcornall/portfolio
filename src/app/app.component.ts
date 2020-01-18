147a7c18362b43c8546e1aba04f6476c60d6335b
tom <tugatom@gmail.com>
2020-01-18 10:09:24 -0800

    Injects theme service to app component to add class dynamically to app element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ThemeService } from './league/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+  isLightTheme: Observable<boolean>;
+
+  constructor(
+    private themeService: ThemeService,
+    private elementRef: ElementRef
+  ) { }
+
+  ngOnInit() {
+    this.themeService.isLightTheme.subscribe(value => {
+        if (value) {
+          this.elementRef.nativeElement.classList.add('light-theme');
+        } else {
+          this.elementRef.nativeElement.classList.remove('light-theme');
+        }
+      }
+    )
+  }
 }
