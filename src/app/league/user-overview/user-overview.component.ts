87addb4c3e154ba6fb53834a92a76ae1ce2baf41
tom <tugatom@gmail.com>
2020-01-18 10:28:59 -0800

    Adds prestigeCrest icon logic and injects theme-service to user-overview

--- src/app/league/user-overview/user-overview.component.ts
+++ src/app/league/user-overview/user-overview.component.ts
@@ -6,6 +6,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { Matchlist } from '../matchlist';
 import { MatchlistMatch } from '../matchlist-match';
+import { ThemeService } from '../theme.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-overview',
@@ -19,28 +21,56 @@ export class UserOverviewComponent implements OnInit {
   account: Account;
   profileIconSource: string;
   defaultProfileIconSource: string;
+  prestigeCrestSource: string;
+  defaultPrestigeCrestSource: string;
   matchlist: Matchlist;
   matchlistMatches: MatchlistMatch[];
+  isLightTheme: Observable<boolean>;
+  prestigeCrestIds = [
+    1,
+    30,
+    50,
+    75,
+    100,
+    125,
+    150,
+    175,
+    200,
+    225,
+    250,
+    275,
+    300,
+    325,
+    350,
+    375,
+    400,
+    425,
+    450,
+    475,
+    500
+  ];
 
   constructor(
     private leagueApiService: LeagueApiService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private themeService: ThemeService
   ) {
-    this.defaultProfileIconSource = `/assets/profileicon/23.png`;
+    this.defaultProfileIconSource = "/assets/profileicon/23.png";
+    this.defaultPrestigeCrestSource = "/assets/prestige-crests/1.png"
   }
 
   ngOnInit() {
+    this.isLightTheme = this.themeService.isLightTheme;
+
     this.route.params.pipe(
       switchMap(params => {
         return this.leagueApiService.getAccount(params.summonerName);
       })
     ).subscribe(
       account => {
-        console.log('got account!###');
-        console.log(account);
         this.account = account;
-        this.setupProfileIcon(account.profileIconId);
+        this.setupProfileIcon(account.profileIconId, account.summonerLevel);
         this.setupUser(account.id);
         this.setupMatches(account.accountId);
       }
@@ -65,14 +95,17 @@ export class UserOverviewComponent implements OnInit {
             switch (entry.queueType) {
               case "RANKED_SOLO_5x5":
                 this.rankedSoloQueue = entry;
+                this.rankedSoloQueue.cardName = "Ranked Solo";
                 this.setupRankedData(this.rankedSoloQueue);
                 break;
               case "RANKED_FLEX_SR":
                 this.rankedFlexSR = entry;
+                this.rankedFlexSR.cardName = "Flex 5v5";
                 this.setupRankedData(this.rankedFlexSR);
                 break;
               case "RANKED_FLEX_TT":
                 this.rankedFlexTT = entry;
+                this.rankedFlexTT.cardName = "Flex 3v3";
                 this.setupRankedData(this.rankedFlexTT);
                 break;
               default:
@@ -92,10 +125,24 @@ export class UserOverviewComponent implements OnInit {
     } else {
       entry.winrate = 100;
     }
+
+    if (entry.winrate < 50) {
+      entry.badWinrate = true;
+    }
   }
 
-  setupProfileIcon(profileIconId: number) {
+  setupProfileIcon(profileIconId: number, summonerLevel: number) {
     this.profileIconSource = `/assets/profileicon/${profileIconId}.png`;
+
+    let crestId = 1;
+
+    this.prestigeCrestIds.forEach(id => {
+      if (summonerLevel >= id) {
+        crestId = id;
+      }
+    });
+
+    this.prestigeCrestSource = `/assets/prestige-crests/${crestId}.png`;
   }
 
   onSubmit(nameInput: string) {
