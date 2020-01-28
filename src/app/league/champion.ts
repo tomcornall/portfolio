b7f4f5eda01845e2c95407613b10577b5f57ff94
tom <tugatom@gmail.com>
2020-01-28 06:39:29 -0800

    Creates champion class

---
+++ src/app/league/champion.ts
@@ -0,0 +1,10 @@
+export class Champion {
+    championId: number;
+    games: number = 0;
+    wins: number = 0;
+    losses: number = 0;
+    imageSource: string;
+    winRatio: number;
+    borderColor: string;
+    championName: string;
+}
